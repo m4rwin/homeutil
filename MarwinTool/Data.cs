234712ac0c7b447a77c0e26f1edd4b1bdb2c974a
marwin <mhromek@trakm8.cz>
2015-05-25 12:53:31 +0100

    nova era commitovani

---
+++ MarwinTool/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarwinTool
+{
+    public class Data : INotifyPropertyChanged
+    {
+        #region INotify
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyChanged)
+        {
+            if (propertyChanged != null)
+            {
+                //PropertyChanged(this, new PropertyChangedEventArgs(propertyChanged));
+            }
+        }
+        #endregion
+
+        #region Properties
+        private DateTime _LastOnlineTime;
+        public DateTime LastOnlineTime
+        {
+            get { return _LastOnlineTime; }
+            set
+            {
+                _LastOnlineTime = value;
+                OnPropertyChanged("LastOnlineTime");
+            }
+        }
+        #endregion
+    }
+}
