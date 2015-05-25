ade3970eec25cdc90767657e8c5a1b1e5f1c5255
marwin <mhromek@trakm8.cz>
2015-05-25 13:11:53 +0100

    nova era commitovani-v1

---
+++ AutoStart/AutostartCore.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoStart
+{
+    public class AutostartCore
+    {
+        public static string GetVersion()
+        {
+            return "0.1";
+        }
+
+        private static RegistryKey onStartup1 = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\", true);
+
+        public static bool SetOnStartup(bool run)
+        {
+            if (run)
+            {
+                try
+                {
+                    onStartup1.SetValue(Application.ProductName, Application.ExecutablePath.ToString());
+                }
+                catch { return false; }
+                return true;
+            }
+            else
+            {
+                try
+                {
+                    onStartup1.DeleteValue(Application.ProductName);
+                }
+                catch { return false; }
+                
+                return true;
+            }
+        }
+
+        public static bool IsOnStartup()
+        {
+            if (onStartup1.GetValue(Application.ProductName) == null) return false;
+
+            if (!System.IO.File.Exists(onStartup1.GetValue(Application.ProductName).ToString()))
+            {
+                onStartup1.SetValue(Application.ProductName, Application.ExecutablePath.ToString());
+            }
+
+            return true;
+        }
+    }
+}
